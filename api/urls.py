a130987fbf946e38c325125361a4d8e9c95e15ad
Jam Risser <jam@jamrizzi.com>
2017-04-04 14:16:14 -0500

    Setparted views

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from api import views
+from api.views import test_view
+from api.views import user_view
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r'users', user_view.UserViewSet)
+router.register(r'groups', user_view.GroupViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^test', views.TestView.as_view())
+    url(r'^test', test_view.TestView.as_view())
 ]
--- api/views/user_view.py
+++ api/views/user_view.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import views, viewsets
+from rest_framework import viewsets
 from rest_framework.response import Response
-from api.serializers import UserSerializer, GroupSerializer
-from api.services import test_service
+from api.serializers.user_serializer import UserSerializer, GroupSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
@@ -11,9 +10,3 @@ class UserViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-
-class TestView(views.APIView):
-    def get(self, request):
-        return Response(test_service.hello_world())
-    def post(self, request):
-        return Response(test_service.hello_world())
